8b8d2216e27cc4228da6bf4a3464bff95dbbd4d4
HolaSoyLuis <luis@Luiss-MacBook-Air.local>
2019-03-19 14:06:41 -0600

    api text in list view

--- frontend/src/components/book/list.vue
+++ frontend/src/components/book/list.vue
@@ -3,7 +3,7 @@
 <div class = "container">
     <div class = "row">
         <div class = "col text-left">
-            <h2>Listado de libros</h2>
+            <h2>Listado de libros (API v1.0)</h2>
             <div class="col-md-12">
               <b-table striped hover :items="books" :fields="fields">
                   <template slot="action" slot-scope="data">
